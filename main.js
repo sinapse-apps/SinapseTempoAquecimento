2216fcceb1e76cf3049bcfc19dc37b02dde5bd23
Romão Felipe Lemanski Barbosa <romaofelipe@outlook.com.br>
2020-02-12 09:44:00 -0200

    Base Design Done

---
+++ main.js
@@ -0,0 +1,15 @@
+const btn_install = document.getElementById("btn_install");
+
+const field_inicial = document.getElementById("field_temp_inicial");
+const field_inicial_error = document.getElementById("field_temp_inicial");
+const field_final = document.getElementById("field_temp_final");
+const field_final_error = document.getElementById("field_temp_final");
+const field_volume = document.getElementById("field_volume");
+const field_volume_error = document.getElementById("field_volume");
+
+const btn_install = document.getElementById("result");
+
+let temp_inicial = 0;
+let temp_final = 0;
+let volume = 0;
+let potencia = 5000;
