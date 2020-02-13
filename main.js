73d5485eb638df6d949fece1378242083d259931
Romão Felipe Lemanski Barbosa <romaofelipe@outlook.com.br>
2020-02-13 08:25:18 -0200

    Interface adjustments

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ if('serviceWorker' in navigator) {
 // Code to handle install prompt on desktop
 
 let deferredPrompt;
-//btn_install.style.display = 'none';
+btn_install.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
@@ -119,15 +119,16 @@ window.addEventListener('beforeinstallprompt', (e) => {
   btn_install.style.display = 'absolute';
 
   btn_install.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
-    //btn_install.style.display = 'none';
+    // hide our user interface that shows our A2HS button    
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
         if (choiceResult.outcome === 'accepted') {
           console.log('User accepted the A2HS prompt');
+          btn_install.style.display = 'none';
         } else {
+          btn_install.style.display = 'absolute';
           console.log('User dismissed the A2HS prompt');
         }
         deferredPrompt = null;
